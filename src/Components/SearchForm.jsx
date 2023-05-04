7a80c30ed1f82d45f010813b2d2716006bcf17d6
Helen Pickavance <helenpjobs@gmail.com>
2023-05-04 18:45:21 +0100

    Input button

--- src/Components/SearchForm.jsx
+++ src/Components/SearchForm.jsx
@@ -1,11 +1,22 @@
-import { Input, InputGroup, InputGroupText, InputProps } from "reactstrap"
+import { useState } from "react";
+import { Button, Input, InputGroup, InputGroupText } from "reactstrap"
 
 const SearchForm = () => {
+  
+    const [movieTitle, setMovieTitle] = useState("");
+    function handleClick() {
+
+        setMovieTitle();
+    }
+
     return (
-        <InputGroup>
-            <InputGroupText>Search</InputGroupText>
-            <Input />
-        </InputGroup>
+        <div style={{padding: "10px"}}>
+            <InputGroup>
+                <InputGroupText>Movie Title</InputGroupText>
+                <Input id={} placeholder="Deadpool"/>
+                <Button onClick={handleClick()}>Search</Button>
+            </InputGroup>
+        </div>
     )
 }
 
