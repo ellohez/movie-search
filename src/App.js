ab0136448aebc19e3fd9d8bf8988595d708806ec
Helen Pickavance <helenpjobs@gmail.com>
2023-05-04 18:22:48 +0100

    Search form inputs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
 import './App.css';
 import MovieInfo from './Components/FilmRequest';
+import SearchForm from './Components/SearchForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <SearchForm />
         <MovieInfo />
       </header>
     </div>
---
+++ src/Components/SearchForm.jsx
@@ -0,0 +1,12 @@
+import { Input, InputGroup, InputGroupText, InputProps } from "reactstrap"
+
+const SearchForm = () => {
+    return (
+        <InputGroup>
+            <InputGroupText>Search</InputGroupText>
+            <Input />
+        </InputGroup>
+    )
+}
+
+export default SearchForm;
